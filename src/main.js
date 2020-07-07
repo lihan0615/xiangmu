0525698008e464ca11653f39f9fdb234f6f2e027
lihan0615 <1445722496@qq.com>
2020-07-07 21:46:54 +0800

    'qvnawang'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+var path = require('path')
+function resolve (dir) {
+  console.log(__dirname)
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+    .set('styles', resolve('src/assets/styles'))
+    .set('common', resolve('src/common'))
+    
+  }
+}
